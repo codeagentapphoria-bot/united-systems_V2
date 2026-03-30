dab05ef64e97efccc8bf2dc24fc4e4196862ae0f
Anivaryam <anivaryam.dev@gmail.com>
2026-03-30 13:57:10 +0800

    fix(auth): trailing newline and parameter naming in jwt.js

--- barangay-information-management-system-copy/server/src/config/jwt.js
+++ barangay-information-management-system-copy/server/src/config/jwt.js
@@ -20,6 +20,6 @@ export const generateRefreshToken = () => {
   return crypto.randomBytes(32).toString('hex');
 };
 
-export const hashRefreshToken = (token) => {
-  return crypto.createHash('sha256').update(token).digest('hex');
+export const hashRefreshToken = (rawToken) => {
+  return crypto.createHash('sha256').update(rawToken).digest('hex');
 };
